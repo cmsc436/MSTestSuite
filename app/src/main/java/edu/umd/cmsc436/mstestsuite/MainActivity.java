60876d71e42f950e11b014afc17226da4a49f355
Thomas Harris <teh019283@gmail.com>
2017-04-18 22:19:26 -0400

    Setting up the basic UI elements

--- app/src/main/java/edu/umd/cmsc436/mstestsuite/MainActivity.java
+++ app/src/main/java/edu/umd/cmsc436/mstestsuite/MainActivity.java
@@ -1,7 +1,9 @@
 package edu.umd.cmsc436.mstestsuite;
 
+import android.support.design.widget.BottomSheetBehavior;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+
+        BottomSheetBehavior b = BottomSheetBehavior.from(findViewById(R.id.bottom_sheet));
+        b.setState(BottomSheetBehavior.STATE_COLLAPSED);
     }
 }
