fa09802ab50445cfac06593d9617944dcfdea8f9
AlexanderYZhang <alexzhangn@gmail.com>
2017-05-01 12:17:57 -0400

    removed TextView import

--- app/src/main/java/edu/umd/cmsc436/mstestsuite/MainActivity.java
+++ app/src/main/java/edu/umd/cmsc436/mstestsuite/MainActivity.java
@@ -21,7 +21,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Arrays;
