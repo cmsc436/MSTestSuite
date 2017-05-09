73f20ddae706d1d96083a93f39c96737a0fe56c7
Thomas Harris <ta.harris.42@gmail.com>
2017-05-09 09:51:04 -0400

    Found an unused import

--- app/src/main/java/edu/umd/cmsc436/mstestsuite/MainActivity.java
+++ app/src/main/java/edu/umd/cmsc436/mstestsuite/MainActivity.java
@@ -28,7 +28,6 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
